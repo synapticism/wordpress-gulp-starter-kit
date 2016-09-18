99171b72014575d4781c32f699565f659c81ff03
Noël Jackson <n@noeljackson.com>
2016-09-18 06:53:31 -0400

    push image

--- scripts/docker-build-for-registry.sh
+++ scripts/docker-build-for-registry.sh
@@ -4,9 +4,11 @@ if [ -z "$1" ]; then
   echo -n "What is your registry tag? ";
   read registry
   docker build -t $registry .;
+  docker push -t $registry;
 else
   echo "Building $1"
   docker build -t "$1" .;
+  docker push -t "$1";
 fi
 
 
