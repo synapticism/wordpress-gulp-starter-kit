91df5ad353db6659d46a0f4ad932822d03e353a1
Noël Jackson <n@noeljackson.com>
2016-10-02 18:44:23 +0200

    Adding safety mechanisms for export and import scripts

--- scripts/docker-exportdb.sh
+++ scripts/docker-exportdb.sh
@@ -1,1 +1,7 @@
-docker-compose exec db bash "/tmp/scripts/export.sh"
+#!/bin/bash
+echo -n "Are you sure you want to EXPORT and OVERWRITE db.sql - y/N? ";
+read confirm
+if [[ "$confirm" == "y" ]]
+then
+  docker-compose exec db bash "/tmp/scripts/export.sh"
+fi
---
+++ scripts/docker-importdb.sh
@@ -0,0 +1,6 @@
+echo -n "Are you sure you want to IMPORT and OVERWRITE db.sql - y/N? ";
+read confirm
+if [[ "$confirm" == "y" ]]
+then
+  docker-compose exec db bash "/tmp/scripts/import.sh"
+fi
